574e1ae45c724cef9bd6433a0dbc8331c744c496
Bohdan Khomych <bod.khomych@gmail.com>
2022-10-14 16:15:34 +0300

    Adds the mobile version of the form

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,17 +2,18 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
+  const bodyScroll = document.querySelector('body');
 
   const toggleMenu = () => {
-    const isMenuOpen =
-      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    const isMenuOpen = openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
+    bodyScroll.classList.toggle('no-scroll');
 
-    const scrollLockMethod = !isMenuOpen
+    /* const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
-    bodyScrollLock[scrollLockMethod](document.body);
+    bodyScrollLock[scrollLockMethod](document.body); */
   };
 
   openMenuBtn.addEventListener('click', toggleMenu);
@@ -22,10 +23,9 @@
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
     if (!e.matches) return;
     mobileMenu.classList.remove('is-open');
+    bodyScroll.classList.remove('no-scroll');
     openMenuBtn.setAttribute('aria-expanded', false);
-    bodyScrollLock.enableBodyScroll(document.body);
+
+    /* bodyScrollLock.enableBodyScroll(document.body); */
   });
 })();
-
-
-
